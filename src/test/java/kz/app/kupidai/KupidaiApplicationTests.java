732593ad9323af04108b48dcebb0224cb7ef9bab
shahruhisaev <isaevshahruh2001@gmail.com>
2026-01-21 21:19:57 +0500

    start project;

---
+++ src/test/java/kz/app/kupidai/KupidaiApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.app.kupidai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KupidaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
