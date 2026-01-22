f7cf9f48ddeaaebe1224aa95298688902df1874d
shahruhisaev <isaevshahruh2001@gmail.com>
2026-01-23 00:14:40 +0500

    kupidai-project -> created docker-compose;

--- services/notifications-service/src/main/java/kz/app/notificationsservice/NotificationsServiceApplication.java
+++ services/notifications-service/src/main/java/kz/app/notificationsservice/NotificationsServiceApplication.java
@@ -2,8 +2,10 @@ package kz.app.notificationsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NotificationsServiceApplication {
 
     public static void main(String[] args) {
