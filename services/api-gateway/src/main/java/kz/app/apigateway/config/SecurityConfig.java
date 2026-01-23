cb48000edd38db0631e0302349942f6ba2529132
shahruhisaev <isaevshahruh2001@gmail.com>
2026-01-23 22:09:30 +0500

    kupidai-project -> gateway is done;

---
+++ services/api-gateway/src/main/java/kz/app/apigateway/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package kz.app.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(authorize -> authorize
+                        .anyExchange().permitAll()  // ВСЁ открыто
+                )
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .build();
+    }
+}
